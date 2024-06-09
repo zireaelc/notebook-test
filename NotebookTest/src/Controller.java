fc77d72765e0a2374d82ed98c2fb1c3816a5785b
zireaelc <gziraelc@gmail.com>
2024-06-09 21:26:31 +0400

    Добавлен поиск по дате

--- NotebookTest/src/Controller.java
+++ NotebookTest/src/Controller.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Controller {
     private RecordStorage storage = new FileRecordStorage();
@@ -25,6 +27,29 @@ public class Controller {
         storage.saveRecords(record);
     }
 
+    private void findRecordsByDate(){
+        System.out.println("Введите дату в формате yyyy-MM-dd:");
+        String dateString = scanner.nextLine().trim();
+        try {
+            LocalDate date = LocalDate.parse(dateString, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            List<Record> foundRecords = records.stream()
+                    .filter(record -> record.getDate().equals(date))
+                    .collect(Collectors.toList());
+
+            if (foundRecords.isEmpty()) {
+                System.out.println("Записей за " + dateString + " не найдено");
+            } else {
+                System.out.println("Найденные записи за " + dateString + " :");
+                for (Record record : foundRecords) {
+                    System.out.println(record.toString());
+                }
+            }
+        }
+        catch (Exception e){
+            System.out.println("Неверный формат даты");
+        }
+    }
+
     public void run(){
         while(true){
             System.out.println("Введите команду: ");
@@ -32,6 +57,7 @@ public class Controller {
             switch (command) {
                 case "#read" -> printAllRecords();
                 case "#write" -> addRecord();
+                case "#find" -> findRecordsByDate();
                 case "#exit" -> {
                     return;
                 }
